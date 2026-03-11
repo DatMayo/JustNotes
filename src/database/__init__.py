e8d4831f6b3abde9ca807a08a64d3e78910601fc
Mario Franze <mario.franze@gmail.com>
2026-03-11 18:18:12 +0100

    refactor: Split CRUD classes into separate modules for better organization
    Reorganize CRUD imports by splitting them into dedicated modules:
    - Import NoteCRUD from note_crud module
    - Import UserCRUD from user_crud module
    - Import TagCRUD from tag_crud module
    - Import KeywordCRUD from keyword_crud module

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,5 +1,8 @@
 from .connection import engine, create_db_and_tables, get_db_session
-from .crud import NoteCRUD, UserCRUD, TagCRUD, KeywordCRUD
+from .note_crud import NoteCRUD
+from .user_crud import UserCRUD
+from .tag_crud import TagCRUD
+from .keyword_crud import KeywordCRUD
 
 __all__ = [
     "engine",
